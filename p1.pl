6ace0bfe11503676e2caa723e181162b44395c33
daltonbr <sacinopatinete@gmail.com>
2016-10-20 11:28:17 -0200

    add new challenges

--- p1.pl
+++ p1.pl
@@ -31,3 +31,48 @@ twice([H|T], [H1|T1]) :- H1 is H * 2, twice(T,T1).
 % sum2 [1,3,5] >>> 9 - [not working - Dalton]
 sum2([],0).
 sum2([H|T],L) :- L is L1 + H, sum(T,L1).
+
+% count 'b' ['a', 'b', 'b', c'] >>> 2
+
+% TODO: make a odd/even function
+odd(1).
+odd(3).
+odd(5).
+odd(7).
+odd(9).
+
+% all_odd([5,3,9]) must be true
+
+poeta(castro_alves).
+poeta(drummond).
+poeta(joao_cabral).
+poeta(mano_brown).
+poeta(arnaldo_antunes).
+morto(drummond).
+morto(joao_cabral).
+morto(castro_alves).
+morto(d_pedro_I).
+
+poeta_morto(X) :- poeta(X), morto(X).
+% some_dead_poet([X]) :- poeta_morto(X).
+some_dead_poet([H|_]) :- poeta_morto(H).
+some_dead_poet([_|T]) :- some_dead_poet(T).
+
+% stammer ['a', 'b', 'c', 'd']  >>> ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd']
+
+% take(3,[1,10,100,1000],[1,10,100]).
+
+% drop(2,[1,10,100,1000],[10,100]).
+
+% alternate([1,3][10,11],[1,10,3,11]).
+
+% max2
+% max2(X,Y,X) :- X > Y.
+% max2(X,Y,Y) :- Y >= X.
+
+% max_filter
+max_filter([3,7,0],7).
+
+% append2([1,2],[3,4],[1,2,3,4],X). %already has a native append command
+
+%substitute(c,d,[a,c,c,e],[a,d,d,e]).
